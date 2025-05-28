658958281ea1bcc3b4db2e736d2a32603cba04ea
Taylan Özgür Özdemir <taylan147701@gmail.com>
2025-05-28 04:42:51 +0300

    cutscenes 3/3 OK

---
+++ Assets/Scripts/SceneControllers/Ch2CutsceneStarter.cs
@@ -0,0 +1,33 @@
+﻿namespace SceneControllers
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    public class Ch2CutsceneStarter : MonoBehaviour
+    {
+        public CutsceneManager cutsceneManager;
+        public Sprite[] cutsceneSprites;
+        public string[] captions;
+
+        private bool _hasPlayed;
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_hasPlayed) return;
+            if (other.CompareTag("Player"))
+            {
+                _hasPlayed = true;
+                StartCutscene();
+            }
+        }
+
+        void StartCutscene()
+        {
+            cutsceneManager.StartCutscene(cutsceneSprites, captions, OnCutsceneComplete);
+        }
+
+        void OnCutsceneComplete()
+        {
+            SceneManager.LoadScene("Chapter 2");
+        }
+    }
+}
